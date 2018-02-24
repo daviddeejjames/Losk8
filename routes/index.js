7ea408224d0c24b0bdc481b7cd0aeea76dbbd0c2
David James <davidj28827@gmail.com>
2018-02-24 21:10:42 +1100

    New icon for spot, removed delisih stuff, start doing CSS

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
-const storeController = require('../controllers/storeController');
+const spotController = require('../controllers/spotController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 const reviewController = require('../controllers/reviewController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
-// Creates our store index/archive pages
-router.get('/', catchErrors(storeController.getStores));
-router.get('/stores', catchErrors(storeController.getStores));
-router.get('/stores/page/:page', catchErrors(storeController.getStores));
+// Creates our spot index/archive pages
+router.get('/', catchErrors(spotController.getSpots));
+router.get('/spots', catchErrors(spotController.getSpots));
+router.get('/spots/page/:page', catchErrors(spotController.getSpots));
 
-// Controls the adding and editing of stores
-router.get('/add', authController.isLoggedIn, storeController.addStore);
+// Controls the adding and editing of spots
+router.get('/add', authController.isLoggedIn, spotController.addSpot);
 
 router.post('/add',
-  storeController.upload,
-  catchErrors(storeController.resize),
-  catchErrors(storeController.createStore)
+  spotController.upload,
+  catchErrors(spotController.resize),
+  catchErrors(spotController.createSpot)
 );
 
 router.post('/add/:id',
-  storeController.upload,
-  catchErrors(storeController.resize),
-  catchErrors(storeController.updateStore)
+  spotController.upload,
+  catchErrors(spotController.resize),
+  catchErrors(spotController.updateSpot)
 );
 
-router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+router.get('/spots/:id/edit', catchErrors(spotController.editSpot));
 
-// The actual store page
-router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
+// The actual spot page
+router.get('/spot/:slug', catchErrors(spotController.getSpotBySlug));
 
-router.get('/tags', catchErrors(storeController.getStoreByTag));
-router.get('/tags/:tag', catchErrors(storeController.getStoreByTag));
+router.get('/tags', catchErrors(spotController.getSpotByTag));
+router.get('/tags/:tag', catchErrors(spotController.getSpotByTag));
 
 // Account
 router.get('/account', authController.isLoggedIn, userController.account);
@@ -65,20 +65,20 @@ router.post('/register',
 router.get('/logout', authController.logout);
 
 // Map
-router.get('/map', storeController.mapPage);
+router.get('/map', spotController.mapPage);
 
 // Hearts
-router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
+router.get('/hearts', authController.isLoggedIn, catchErrors(spotController.getHearts));
 
 // Reviews
 router.post('/reviews/:id', authController.isLoggedIn, catchErrors(reviewController.addReview));
 
 // Top
-router.get('/top', catchErrors(storeController.getTopStores));
+router.get('/top', catchErrors(spotController.getTopSpots));
 
 // API
-router.get('/api/search', catchErrors(storeController.searchStores));
-router.get('/api/stores/near', catchErrors(storeController.mapStores));
-router.post('/api/stores/:id/heart', catchErrors(storeController.heartStore));
+router.get('/api/search', catchErrors(spotController.searchSpots));
+router.get('/api/spots/near', catchErrors(spotController.mapSpots));
+router.post('/api/spots/:id/heart', catchErrors(spotController.heartSpot));
 
 module.exports = router;
